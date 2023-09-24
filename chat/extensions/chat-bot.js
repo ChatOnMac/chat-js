e1e1e88f0e621e08b3d9b009ce521a81810433b8
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-24 15:44:06 -0400

    wip

--- chat/extensions/chat-bot.js
+++ chat/extensions/chat-bot.js
@@ -1,6 +1,6 @@
 // This will be cleaned up for easier reuse soon. --ChatOnMac
 
-import { Chat } from "jsdelivr.gh:ChatOnMac/chat-js@5250182/chat/modules/chat.js";
+import { Chat } from "jsdelivr.gh:ChatOnMac/chat-js@35e127e/chat/modules/chat.js";
 // import { Chat } from "https://github.com/ChatOnMac/chat-js/blob/main/chat/modules/chat.js";
 
 // Copied from module

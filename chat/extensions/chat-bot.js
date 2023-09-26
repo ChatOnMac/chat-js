6d4f37819f29301e07b4d34a3bb7e863e60e1b21
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-25 20:11:36 -0400

    wip

--- chat/extensions/chat-bot.js
+++ chat/extensions/chat-bot.js
@@ -15,16 +15,16 @@
 
 // import { proxyConsole } from "jsdelivr.gh:ChatOnMac/chat-js@main/chat/modules/console-proxy.js";
 
-import { addRxPlugin, createRxDatabase, lastOfArray, deepEqual } from "esm.run:rxdb";
-import { RxDBDevModePlugin } from "esm.run:rxdb/plugins/dev-mode";
-import { replicateRxCollection } from "esm.run:rxdb/plugins/replication";
-import { getRxStorageMemory } from "esm.run:rxdb/plugins/storage-memory";
-import { createDeferredExecutor } from "esm.run:@open-draft/deferred-promise";
+import { addRxPlugin, createRxDatabase, lastOfArray, deepEqual } from "skypack:rxdb";
+import { RxDBDevModePlugin } from "skypack:rxdb/plugins/dev-mode";
+import { replicateRxCollection } from "skypack:rxdb/plugins/replication";
+import { getRxStorageMemory } from "skypack:rxdb/plugins/storage-memory";
+/*import { createDeferredExecutor } from "esm.run:@open-draft/deferred-promise";
 import { until } from "esm.run:@open-draft/until";
 import { Emitter } from "esm.run:strict-event-emitter";
 import { Logger } from "esm.run:@open-draft/logger";
 import { invariant } from "esm.run:outvariant";
-import { isNodeProcess } from "esm.run:is-node-process";
+import { isNodeProcess } from "esm.run:is-node-process";*/
 // import { BatchInterceptor } from 'esm.run:@mswjs/interceptors@0.25.4';
 // import browserInterceptors from 'esm.run:@mswjs/interceptors@0.25.4/lib/browser/presets/browser.mjs';
 

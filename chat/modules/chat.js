bfff73f0b36f0b58968839f98e5c8c28585d9cd1
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-26 00:09:10 -0400

    wip

--- chat/modules/chat.js
+++ chat/modules/chat.js
@@ -268,7 +268,7 @@ class Chat extends EventTarget {
         var rooms = rooms || await this.db.collections.room.find().exec();
         const botsInRoomsIDs = [...new Set(rooms.flatMap(room => room.participants))];
         const botsInRooms = await this.db.collections.persona.findByIds(botsInRoomsIDs).exec();
-        const unusedOnlineBots = await this.db.collections.persona.find({ selector: { id: { $not: { $in: botsInRoomsIDs } } } }).exec();
+        const unusedOnlineBots = await this.db.collections.persona.find({ selector: { online: true, id: { $not: { $in: botsInRoomsIDs } } } }).exec();
         // await offerUnusedPersonas({ botsInRooms, unusedOnlineBots });
         this.dispatchEvent(new CustomEvent("offerUnusedPersonas", { detail: { db: this.db, botsInRooms, unusedOnlineBots } }));
     }

1344766720c2b9487cd7a6b3c5fe58c7f40dc8c4
Alex Ehlke <alex.ehlke@gmail.com>
2023-09-28 23:45:31 -0400

    wip

--- chat/extensions/chat-bot.js
+++ chat/extensions/chat-bot.js
@@ -178,11 +178,9 @@ class ChatParentBridge {
     }
     
     async createCollectionsFromCanonical(collections) {
-        console.log("create CAn From Can")
         for (const [collectionName, collection] of Object.entries(collections)) {
             collections[collectionName]["conflictHandler"] = conflictHandler;
         }
-        console.log(collections)
         await this.db.addCollections(collections);
 
         const collectionEntries = Object.entries(this.db.collections);
@@ -196,7 +194,6 @@ class ChatParentBridge {
             replicationState.reSync();
             await replicationState.awaitInSync();
         }
-        console.log("create CAn From Can - ova")
     }
 
     async syncDocsFromCanonical(collectionName, changedDocs) {
@@ -223,14 +220,12 @@ class ChatParentBridge {
     }
 
     async finishedSyncingDocsFromCanonical() {
-        console.log("finishedSyncingDocsFromCan()")
         for (const replicationState of Object.values(this.state.replications)) {
             replicationState.reSync();
         }
         await this.replicationInSync()
     
         await this.onFinishedSyncingDocsFromCanonical();
-        console.log("eh2")
     }
 }
 
@@ -249,16 +244,12 @@ class Chat extends EventTarget {
     }
 
     async onFinishedSyncingDocsFromCanonical() {
-        console.log("on finish 1")
         this.dispatchEvent(new CustomEvent("finishedInitialSync", { detail: { db: this.db, replications: this.state.replications } }));
-        console.log("on finish 2")
         await this.keepOwnPersonasOnline();
-        console.log("on finish 3")
         // this.offerUnusedPersonas = this.offerUnusedPersonas.bind(this);
         // await this.offerUnusedPersonas();
         // this.dispatchEvent(new CustomEvent("offerUnusedPersonas", { detail: { } }));
         await this.wireUnusedPersonas();
-        console.log("on finish 4")
     }
 
     static async init() {
@@ -342,8 +333,6 @@ class Chat extends EventTarget {
             return bots;
         }
     
-        //console.log(this.db.collections["persona"])
-//console.log(this.db.collections["persona"].findOne({ selector: { personaType: "bot" } }))
         const botPersona = await this.db.collections.persona
             .findOne({ selector: { personaType: "bot" } })
             .exec();

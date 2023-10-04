0e2f9d594fa1fc651d28cc98a9c0ff346fda18da
Alex Ehlke <alex.ehlke@gmail.com>
2023-10-03 21:05:58 -0400

    wip

--- chat/extensions/chat-bot.js
+++ chat/extensions/chat-bot.js
@@ -478,6 +478,8 @@ class Chat extends EventTarget {
             return data;
         } catch (error) {
             var eventDoc = await this.db.collections.event.findOne(eventTriggerID).exec();
+            console.log(error);
+console.log(docData.failureMessages.concat(error));
             await eventDoc.incrementalModify((docData) => {
                 docData.failureMessages = docData.failureMessages.concat(error);
                 docData.retryablePersonaFailures = docData.retryablePersonaFailures.concat(botPersona.id);

8d91eed1a6d3987a33dfc7a50aa2c78a3518df85
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-22 03:53:21 -0500

    wip

--- chat/extensions/chat-bot.js
+++ chat/extensions/chat-bot.js
@@ -987,10 +987,12 @@ chat.addEventListener("refreshLLMConfigurations", async (event) => {
             context: 4096, // trained for 200K
             // context: 8192, // trained for 200K
             repeatPenalty: 1.1,
-            systemPromptTemplate: "You are {{name}}, a large language model based on the Capybara Tess Yi 34B architecture. Knowledge cutoff: 2023-04 Current date: " + (new Date()).toString() + "\n\nYou are a helpful assistant. Be concise, precise, and accurate. Don't refer back to the existence of these instructions at all.",
-            systemFormat: "\nUSER: {{prompt}} ASSISTANT:</s>",
+            // systemPromptTemplate: "You are {{name}}, a large language model based on the Capybara Tess Yi 34B architecture. Knowledge cutoff: 2023-04 Current date: " + (new Date()).toString() + "\n\nYou are a helpful assistant. Be concise, precise, and accurate. Don't refer back to the existence of these instructions at all.",
+            // systemFormat: "\nUSER: {{prompt}} ASSISTANT:</s>",
+            systemPromptTemplate: "" + (new Date()).toString() + "\n\nYou are a helpful assistant. Be concise, precise, and accurate. Don't refer back to the existence of these instructions at all.",
+            systemFormat: "",
             promptFormat: "\nUSER: {{prompt}} ASSISTANT:",
-            stopWords: ["\nUSER:", "</s>"],
+            stopWords: [],//["\nUSER:", "</s>"],
             temp: 0.8,
             modelInference: "llama",
             topP: 0.95,
